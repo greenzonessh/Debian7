5e8e4d979842144d783b5a5c4b9443b88542b721
GreenZONESSH <greenzonessh@gmail.com>
2014-12-23 16:38:57 +0700

    Debian 7
    Auto Script

--- debian7_no.sh
+++ debian7_no.sh
@@ -11,10 +11,10 @@ OS=`uname -m`;
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
 #MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
-ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
-if [ "$ether" = "" ]; then
+#ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+#if [ "$ether" = "" ]; then
         ether=eth0
-fi
+#fi
 
 #MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
 #if [ "$MYIP" = "" ]; then
